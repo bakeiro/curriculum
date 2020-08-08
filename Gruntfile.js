6133c39f01e58cc661771f69fdf3917258b6f10f
root <root@DESKTOP-CS5B54K.localdomain>
2020-08-08 16:16:33 +0200

    message

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         options: {
           patterns: [
             {
-              json: {}
+              json: {"a": "b"}
             }
           ]
         },
@@ -30,17 +30,21 @@ module.exports = function (grunt) {
       let temp_pattern = new RegExp(key, 'g');
       new_config[temp_pattern] = dictionary_words_es[key];
     }
+
+    let new_config_array = [{json: new_config}];
     
-    grunt.config.set('replace.dist.options.patterns[0].json', new_config);
+    console.log(grunt.config.get('replace.dist.options.patterns'));
+    grunt.config.set('replace.dist.options.patterns', new_config_array);
+    console.log(grunt.config.get('replace.dist.options.patterns'));
     
     let dist_path = `build/${locale_lang}/`;
     if (locale_lang === 'es') {
       dist_path = `build/`;
     }
 
-    grunt.config.set('replace.dist.files.dest', dist_path);
+    //grunt.config.set('replace.dist.files.dest', dist_path);
 
-    grunt.task.run('replace');
+    grunt.task.run('replace:dist');
   });
 
   grunt.registerTask('default', ['buildSite:es']);
