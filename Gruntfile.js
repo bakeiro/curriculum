9467a77764a4707fdcfc0f71a96accf9cb9b2ff2
root <root@DESKTOP-CS5B54K.localdomain>
2020-08-08 15:48:11 +0200

    progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,11 @@ module.exports = function (grunt) {
     replace: {
       dist: {
         options: {
-          replacements: []
+          patterns: [
+            {
+              json: {}
+            }
+          ]
         },
         files: {
           expand: true, src: ['src/index.html'], dest: 'build/'
@@ -20,26 +24,24 @@ module.exports = function (grunt) {
     console.log(`building ${locale_lang} site...`);
 
     let dictionary_words_es = grunt.file.readJSON(`src/lang/${locale_lang}-words.json`);
-    let new_config;
+    let new_config = {};
     
     for (key in dictionary_words_es) {
-      new_config.push({
-        pattern: new RegExp(key, 'g'),
-        replacement: dictionary_words_es[key]
-      });
+      let temp_pattern = new RegExp(key, 'g');
+      new_config[temp_pattern] = dictionary_words_es[key];
     }
     
-    grunt.config.set('string-replace.dist.options.replacements', new_config);
+    grunt.config.set('replace.dist.options.patterns[0].json', new_config);
     
     let dist_path = `build/${locale_lang}/`;
     if (locale_lang === 'es') {
       dist_path = `build/`;
     }
 
-    grunt.config.set('string-replace.dist.files.dest', dist_path);
+    grunt.config.set('replace.dist.files.dest', dist_path);
 
-    grunt.task.run('string-replace:dist');
+    grunt.task.run('replace');
   });
 
-  grunt.registerTask('default', ['buildSite:es', 'buildSite:en']);
+  grunt.registerTask('default', ['buildSite:es']);
 };
